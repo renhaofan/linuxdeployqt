4ed4cb72cbe37635e6cf1e9ed475a3fb27a6d8d7
renhaofan <yqykrhf@163.com>
2022-01-15 09:33:45 +0800

    for ubuntu20.04

--- tools/linuxdeployqt/main.cpp
+++ tools/linuxdeployqt/main.cpp
@@ -200,15 +200,15 @@ int main(int argc, char **argv)
         // openSUSE Leap 15.0 uses glibc 2.26 and is used on OBS
         // Ubuntu Xenial (16.04) uses glibc 2.23
         // Ubuntu Bionic (18.04) uses glibc 2.27
-        if (strverscmp (glcv, "2.28") >= 0) {
-            qInfo() << "ERROR: The host system is too new.";
-            qInfo() << "Please run on a system with a glibc version no newer than what comes with the oldest";
-            qInfo() << "currently still-supported mainstream distribution (Ubuntu Bionic), which is glibc 2.27.";
-            qInfo() << "This is so that the resulting bundle will work on most still-supported Linux distributions.";
-            qInfo() << "For more information, please see";
-            qInfo() << "https://github.com/probonopd/linuxdeployqt/issues/340";
-            return 1;
-        }
+//        if (strverscmp (glcv, "2.28") >= 0) {
+//            qInfo() << "ERROR: The host system is too new.";
+//            qInfo() << "Please run on a system with a glibc version no newer than what comes with the oldest";
+//            qInfo() << "currently still-supported mainstream distribution (Ubuntu Bionic), which is glibc 2.27.";
+//            qInfo() << "This is so that the resulting bundle will work on most still-supported Linux distributions.";
+//            qInfo() << "For more information, please see";
+//            qInfo() << "https://github.com/probonopd/linuxdeployqt/issues/340";
+//            return 1;
+//        }
     }
 
     if (argc < 2 || (firstArgument.startsWith("-"))) {
